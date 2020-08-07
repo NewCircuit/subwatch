1e2d4f2e6c8c3495f099faad8fbd129dbc45cb86
Dylan Hackworth <dhpf@pm.me>
2020-08-07 02:43:11 -0500

    Fixed duration

--- internal/config.go
+++ internal/config.go
@@ -13,7 +13,7 @@ type Config struct {
 	Guild               string        `yaml:"guild"`
 	Roles               []string      `yaml:"roles"`
 	Auth                string        `yaml:"auth_server"`
-	Intervals           int           `yaml:"intervals"`
+	Intervals           time.Duration `yaml:"intervals"`
 	Delay               time.Duration `yaml:"kick_delay"`
 	UpVote              string        `yaml:"up_vote"`
 	DownVote            string        `yaml:"down_vote"`
@@ -25,8 +25,8 @@ func GetConfig(location string) (config Config) {
 	config = Config{
 		Prefix:    ".subwatch",
 		Roles:     []string{"1", "2", "3", "4"},
-		Intervals: 5,
-		Delay:     10,
+		Intervals: time.Minute * 5,
+		Delay:     time.Second * 10,
 	}
 	err := util.GetConfig(location, &config)
 
